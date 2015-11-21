8ed8d7a146954aac7647e07610861655e02e4069
Faishal Hussain Siddiqui <faishalhussain22@gmail.com>
2015-11-21 10:37:05 +0530

    mm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
+set :application, 'mybus_app'
 set :repo_url, 'git@github.com:faishal28/mybus_app.git'
+set :scm, :git
 set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :deploy_to, '/home/deploy/mybus'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
